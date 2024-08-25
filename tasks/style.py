95b0c171b4fae3a0339a8cbd165817415680af4b
Wei Lee <weilee.rx@gmail.com>
2024-08-25 19:36:02 +0800

    feat: rewrite the whole package as markdown-mermaid

---
+++ tasks/style.py
@@ -0,0 +1,52 @@
+from invoke.context import Context
+from invoke.tasks import task
+
+from tasks.common import COMMON_TARGETS_AS_STR, VENV_PREFIX
+
+
+@task
+def ruff(ctx: Context) -> None:
+    """Check style through ruff"""
+    ctx.run(f"{VENV_PREFIX} ruff check {COMMON_TARGETS_AS_STR}")
+
+
+@task
+def mypy(ctx: Context) -> None:
+    """Check style through mypy"""
+    ctx.run(f"{VENV_PREFIX} mypy")
+
+
+@task
+def black_check(ctx: Context) -> None:
+    """Check style through black"""
+    ctx.run(f"{VENV_PREFIX} black --check {COMMON_TARGETS_AS_STR}")
+
+
+@task
+def commit_check(ctx: Context, remote: str = "origin") -> None:
+    """Check commit message through commitizen"""
+    ctx.run(f"{VENV_PREFIX} cz -nr 3 check --rev-range {remote}/main..", warn=True)
+
+
+@task(pre=[ruff, mypy, black_check, commit_check], default=True)
+def run(ctx: Context) -> None:
+    """Check style through linter (Note that pylint is not included)"""
+    pass
+
+
+@task
+def black(ctx: Context) -> None:
+    """Format Python code through Black"""
+    ctx.run(f"{VENV_PREFIX} black {COMMON_TARGETS_AS_STR}")
+
+
+@task
+def ruff_format(ctx: Context) -> None:
+    """Format Python code through ruff"""
+    ctx.run(f"{VENV_PREFIX} ruff format {COMMON_TARGETS_AS_STR}")
+
+
+@task(pre=[black, ruff_format])
+def format(ctx: Context) -> None:
+    """Reformat python files through black and ruff"""
+    pass
